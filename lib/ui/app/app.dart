3cb3f72cfefa176bc5e32a80acdca051f4fbc8ee
Michael Kolchanov <01gicha01@gmail.com>
2024-06-09 00:28:18 +0400

    connect app to telegram mini app

--- lib/ui/app/app.dart
+++ lib/ui/app/app.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fton/ui/pages/start/start_view.dart';
+import 'package:telegram_web_app/telegram_web_app.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -13,20 +11,9 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   @override
-  Widget build(BuildContext context) {
-    if (Platform.isAndroid) {
-      return const MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: StartViewPage(),
+        theme: TelegramThemeUtil.getTheme(TelegramWebApp.instance),
+        home: const StartViewPage(),
       );
-    } else {
-      return const CupertinoApp(
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: [DefaultMaterialLocalizations.delegate],
-        home: ScaffoldMessenger(
-          child: StartViewPage(),
-        ),
-      );
-    }
-  }
 }
