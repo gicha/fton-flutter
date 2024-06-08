9fd99c67648f9587b325970c4307d9f418594329
Michael Kolchanov <01gicha01@gmail.com>
2024-06-09 00:45:35 +0400

    add tg theme

--- lib/ui/app/app.dart
+++ lib/ui/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fton/ui/pages/start/start_view.dart';
 import 'package:telegram_web_app/telegram_web_app.dart';
@@ -11,9 +12,32 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   @override
-  Widget build(BuildContext context) => MaterialApp(
+  Widget build(BuildContext context) {
+    final isAndroid = TelegramWebApp.instance.platform == 'android';
+    final isLight = TelegramWebApp.instance.colorScheme.value == 'light';
+    final theme = TelegramThemeUtil.getTheme(TelegramWebApp.instance);
+    if (isAndroid) {
+      return MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: TelegramThemeUtil.getTheme(TelegramWebApp.instance),
+        theme: theme,
+        title: 'FTON',
+        themeMode: isLight ? ThemeMode.light : ThemeMode.dark,
         home: const StartViewPage(),
       );
+    } else {
+      return CupertinoApp(
+        debugShowCheckedModeBanner: false,
+        theme: theme == null
+            ? null
+            : MaterialBasedCupertinoThemeData(materialTheme: theme),
+        title: 'FTON',
+        localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+          DefaultMaterialLocalizations.delegate,
+          DefaultCupertinoLocalizations.delegate,
+          DefaultWidgetsLocalizations.delegate,
+        ],
+        home: const StartViewPage(),
+      );
+    }
+  }
 }
