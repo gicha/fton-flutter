0d86003b2873eba490faaa0b262520166784edcf
Michael Kolchanov <01gicha01@gmail.com>
2024-06-11 03:33:04 +0400

    fix ton connect

--- lib/ui/widgets/ton_connect_button/ton_connect_button_wm.dart
+++ lib/ui/widgets/ton_connect_button/ton_connect_button_wm.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:darttonconnect/exceptions.dart';
 import 'package:darttonconnect/models/wallet_app.dart';
 import 'package:darttonconnect/ton_connect.dart';
 import 'package:flutter/material.dart';
 import 'package:fton/model/ton_connect/ton_connect_storage.dart';
 import 'package:fton/ui/widgets/ton_connect_button/ton_connect_button.dart';
+import 'package:http/http.dart' as http;
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:telegram_web_app/telegram_web_app.dart';
 
@@ -12,8 +15,7 @@ enum TonConnectStatus {
   loadingWallets('Loading...'),
   readyToConnect('Connect'),
   connecting('Connecting'),
-  connected('Disconnect'),
-  ;
+  connected('Disconnect');
 
   const TonConnectStatus(this.title);
 
@@ -59,19 +61,67 @@ mixin TonConnectButtonWm on State<TonConnectButton> {
   }
 
   Future<void> initialConnect() async {
-    wallets = await connector.getWallets();
-    wallets!.add(
-      const WalletApp(
-        name: 'Wallet',
-        image: 'https://wallet.tg/images/logo-288.png',
-        aboutUrl: 'https://wallet.tg/',
-        universalUrl: 'https://t.me/wallet?attach=wallet',
-        bridgeUrl: 'https://bridge.ton.space/bridge',
-      ),
-    );
+    wallets = await getWallets();
     setState(() {
       status = TonConnectStatus.readyToConnect;
     });
+  }
+
+  Future<List<WalletApp>> getWallets() async {
+    final List<WalletApp> walletsList = [];
+    try {
+      final response = await http.get(
+        Uri.parse(
+          'https://raw.githubusercontent.com/ton-blockchain/wallets-list/main/wallets.json',
+        ),
+      );
+      if (response.statusCode == 200) {
+        final responseBody = jsonDecode(response.body);
+        if (responseBody is List) {
+          for (final walletJson in responseBody) {
+            final wallet = fromMap(walletJson as Map<String, dynamic>);
+            if (wallet.bridgeUrl.isNotEmpty) {
+              walletsList.add(wallet);
+            }
+          }
+        } else {
+          throw FetchWalletsError(
+            'Wrong wallets list format, wallets list must be an array.',
+          );
+        }
+      } else {
+        throw FetchWalletsError('Failed to fetch wallets list.');
+      }
+    } catch (_) {}
+    return walletsList;
+  }
+
+  WalletApp fromMap(Map<String, dynamic> json) {
+    final String bridgeUrl = () {
+      if (json.containsKey('bridge_url')) {
+        return json['bridge_url'].toString();
+      }
+      if (json.containsKey('bridge')) {
+        final bridges = (json['bridge'] as List<dynamic>)
+            .map((e) => e as Map<String, dynamic>);
+        final bridge = bridges.firstWhere(
+          (bridge) => bridge['type'] == 'sse',
+          orElse: () => {'url': ''},
+        );
+        return bridge['url'].toString();
+      }
+      return '';
+    }();
+
+    return WalletApp(
+      name: json['name'].toString(),
+      image: json['image'].toString(),
+      bridgeUrl: bridgeUrl,
+      aboutUrl: json['about_url'].toString(),
+      universalUrl: json.containsKey('universal_url')
+          ? json['universal_url'].toString()
+          : null,
+    );
   }
 
   void onButtonTap() {
@@ -88,9 +138,9 @@ mixin TonConnectButtonWm on State<TonConnectButton> {
     }
   }
 
-  Future<void> connect() async {
+  Future<WalletApp?> selectWallet() async {
     final wallets = this.wallets!;
-    final selectedWallet = await showDialog<WalletApp>(
+    return showDialog<WalletApp>(
       // ignore: use_build_context_synchronously
       context: context,
       builder: (context) => Center(
@@ -125,10 +175,14 @@ mixin TonConnectButtonWm on State<TonConnectButton> {
         ),
       ),
     );
+  }
+
+  Future<void> connect() async {
+    final selectedWallet = await selectWallet();
     if (selectedWallet == null) return;
     final url = await connector.connect(selectedWallet);
     if (TelegramWebApp.instance.isSupported) {
-      await TelegramWebApp.instance.openLink(url);
+      await TelegramWebApp.instance.openLink(url, tryInstantView: false);
     } else {
       await showAdaptiveDialog(
         // ignore: use_build_context_synchronously
